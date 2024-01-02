641df1c8dd8eb6cacac9dbb5d289c128772460d9
Ayush <ayushguptass14@gmail.com>
2024-01-02 22:33:25 +0530

    okaay

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import React, { useState } from 'react';
+
 function Contact() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(apiEndpoint);
+      const data = await response.json();
+      setApiMessage(data.message);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setApiMessage('Error fetching data');
+    }
+  };
   return (
     <Layout>
-        <h1>contact page </h1>
+      
+  const [apiMessage, setApiMessage] = useState('');
+  const apiEndpoint = 'https://tiny-jade-cougar-cap.cyclic.app/hello';
+  <h1>API Call Example</h1>
+      <button onClick={fetchData}>Fetch Data</button>
+      {apiMessage && (
+        <div>
+          <h2>API Response:</h2>
+          <pre>{JSON.stringify({ message: apiMessage }, null, 2)}</pre>
+        </div>
+      )}
 
     </Layout>
   )
