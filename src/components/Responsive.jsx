6f1c1cb31b144f15c986f02d7abd5d4d77c02539
Ayush <ayushguptass14@gmail.com>
2024-01-02 21:15:17 +0530

    okay

--- src/components/Responsive.jsx
+++ src/components/Responsive.jsx
@@ -18,41 +18,41 @@ function Responsive() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto px-32 text-white">
-              <Link href="/features" className="text-white">Features</Link>
-              <Link href="/about" className="text-white">About</Link>
+              <Link to="/features" className="text-white px-3">Features</Link>
+              <Link to="/about" className="text-white">About</Link>
              
-              <Link href="/contact" className="text-white">Contact Us</Link>
-              <Link href="/booking" className="text-white">        <button className='bg-yellow-300  mt-[1px] rounded-md h-7 w-24 text-[20px]  hover:bg-yellow-500'><p className='font-semibold pb-[3px] text-gray-600 hover:text-white '>Book Now</p></button>
+              <Link to="/contact" className="text-white px-3">Contact Us</Link>
+              <Link to="/booking" className="text-white px-3">        <button className='bg-yellow-300  mt-[1px] rounded-md h-7 w-24 text-[20px]  hover:bg-yellow-500'><p className='font-semibold pb-[3px] text-gray-600 hover:text-white '>Book Now</p></button>
 </Link>
 
 
               {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item to="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item to="#action/3.2">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item to="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item to="#action/3.4">
                 Separated link
               </NavDropdown.Item>
             </NavDropdown> */}
             </Nav>
             <Nav>
          
-              <Link eventKey={2} href="/sign-up">
-             <div className="flex " >
-              <PiSignInBold class="signupbar" className="mt-[16px] text-white" />
+              <Link eventKey={2} to="/sign-up">
+             <div className="flex px-3" >
+              <PiSignInBold class="signupbar" className="mt-[8px] text-white" />
               <Link to="/login" className="text-white">Login</Link>
 
              </div>
              
      
               </Link>
-              <Link eventKey={2} href="/sign-up">
-             <div className="flex " >
-              <PiSignInBold class="signupbar" className="mt-[16px] text-white" />
-              <Link href="/sign-up" className="text-white">Sign Up</Link>
+              <Link eventKey={2} to="/sign-up">
+             <div className="flex px-3 " >
+              <PiSignInBold class="signupbar" className="mt-[8px] text-white" />
+              <Link to="/sign-up" className="text-white">Sign Up</Link>
 
              </div>
              
